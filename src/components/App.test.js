ebffef538c749fed5716b27632d635249a3eb721
juancarlosfarah <juancarlos.farah@pm.me>
2020-03-19 12:21:22 +0100

    build: do not build single file version

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -12,6 +12,7 @@ describe('<App />', () => {
     dispatchGetContext: jest.fn(),
     dispatchGetAppInstance: jest.fn(),
     dispatchGetAppInstanceResources: jest.fn(),
+    dispatchGetCurrentUser: jest.fn(),
   };
   // eslint-disable-next-line react/jsx-props-no-spreading
   const component = shallow(<App {...props} />);
